1b33edd81f1862509aa5fc6307f72b859b135518
abhijitganeshshinde <abhijitganeshshinde@gamil.com>
2020-05-26 23:00:26 +0530

    Added : User And Car Parking Registration In Controllers

--- ParkingLot/Controllers/ParkingLotsController.cs
+++ ParkingLot/Controllers/ParkingLotsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Interface;
+using CommonLayer.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,44 @@ namespace ParkingLot.Controllers
     [ApiController]
     public class ParkingLotsController : ControllerBase
     {
+        public readonly IParkingLotBL parkingLotBL;
+
+        public ParkingLotsController(IParkingLotBL _parkingLotBL)
+        {
+            parkingLotBL = _parkingLotBL;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult AddParkingDetail(ParkingLotDetails parkingLot)
+        {
+            var data = parkingLotBL.AddData(parkingLot);
+            bool success = true;
+            string message;
+            message = "Registration Successfully";
+            return Ok(new { success, message, data });
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult GetAllParkingDetail()
+        {
+            var data = parkingLotBL.GetAllParkingLotData();
+            bool success = true;
+            string message;
+            message = "Successfull Get All Parking Data";
+            return Ok(new { success, message, data });
+        }
+
+        [HttpPost]
+        [Route("UnPark")]
+        public ActionResult UnPark(VehicleUnPark vehicleUnPark)
+        {
+            var data = parkingLotBL.vehicleUnPark(vehicleUnPark);
+            bool success = true;
+            string message;
+            message = "Successfully UnPark";
+            return Ok(new { success, message, data });
+        }
     }
 }
--- ParkingLot/Controllers/UserController.cs
+++ ParkingLot/Controllers/UserController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Interface;
+using CommonLayer.Models;
+using CommonLayer.Responce;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +15,103 @@ namespace ParkingLot.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserBL userBL;
+
+        public UserController(IUserBL _userBL)
+        {
+            userBL = _userBL;
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        [Authorize(Roles = "Admin")]
+        public ActionResult GetAllUserData()
+        {
+            var data = userBL.GetAllUserData();
+            bool success = true;
+            string message;
+            message = "Successfully Get All User Data";
+            return Ok(new { success, message, data });
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult AddUserDetail(UserRegistration registration)
+        {
+            var data = userBL.AddUserData(registration);
+            bool success = true;
+            string message;
+            message = "Registration Successfully";
+            return Ok(new { success, message, data });
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public ActionResult Login(Login registration)
+        {
+            var data = userBL.Login(registration);
+            bool success = false;
+            string message;
+
+            if (registration.User_Type.Contains("Admin"))
+            {
+                success = true;
+                message = "Login Successfully With Admin Privilege ";
+                return Ok(new { success, message, data });
+            }
+            else if (registration.User_Type.Contains("Police"))
+            {
+                success = true;
+                message = "Login Successfully With Police Privilege ";
+                return Ok(new { success, message, data });
+            }
+            else if (registration.User_Type.Contains("Security"))
+            {
+                success = true;
+                message = "Login Successfully With Security Privilege ";
+
+                if (data.Equals(2))
+                {
+                    // message = "Parking Full";
+                    string status = "Parking Full";
+                    return Ok(new { success, message, status });
+                }
+                else
+                {
+                    string status = "Parking Available";
+                    return Ok(new { success, message, status });
+                }
+
+            }
+            else
+            {
+                success = true;
+                message = "Login Successfully With Driver Privilege ";
+                return Ok(new { success, message, data });
+            }
+        }
+
+        [HttpGet]
+        [Route("count")]
+        public ActionResult Count()
+        {
+            var data = userBL.Count();
+            bool success = false;
+            string message;
+
+            if (data.Equals(2))
+            {
+                success = true;
+                message = "Parking Full";
+                return Ok(new { success, message });
+            }
+            else
+            {
+                success = true;
+                message = "Parking Avaliable";
+                return Ok(new { success, message });
+            }
+        }
     }
 }
